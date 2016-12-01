85b6d67d1a4ae65f83ed71c607de96fe97ae4c78
alberto <alberto.rdrz@gmail.com>
2016-12-02 00:34:04 +0100

    basic_* amd tests pass

---
+++ README.md
@@ -0,0 +1,6 @@
+# aimed
+
+A light
+[AMD](https://github.com/amdjs/amdjs-api/wiki/AMD) implemetation, compliant with the following parts of the spec:
+* (basic)[https://github.com/amdjs/amdjs-tests#basic-amd-functionality-basic]
+* (require)[https://github.com/amdjs/amdjs-tests#the-basic-require-method-require]
---
+++ aimed.js
@@ -0,0 +1,124 @@
+(function(root) {
+  function require(deps, cb) {
+    if (typeof deps == "string") {
+      var module = modules[deps]
+      return invokeModule(module);
+    }
+    requireAll(deps, cb);
+  }
+
+  var modules = {
+    "require": {
+      factory: function() { return require; }
+    },
+    "module": {
+      factory: function() { return {}; }
+    },
+    "exports": {
+      factory: function() { return {} }
+    }
+  };
+
+  require.toUrl = function(path) {
+    return path;
+  }
+
+  root.require = require;
+
+  root.define = function define(id, deps, factory) {
+    if (!factory) {
+      factory = deps;
+      deps = ["require", "exports", "module"];
+    }
+    modules[id] = {
+      id: id,
+      factory: factory,
+      deps: deps
+    };
+  }
+
+  root.define.amd = {};
+
+  function requireAll(deps, factory) {
+    var count = 0;
+    function ready() {
+      count++;
+      if (count === deps.length) {
+        var solved = solve(deps);
+        invoke(factory, solved);
+      }
+    }
+
+    deps.forEach(function(dep) {
+      requireSingle(dep, ready);
+    });
+  }
+
+  function requireSingle(dep, cb) {
+    var module = modules[dep];
+    if (module) {
+      if (module.deps && module.deps.length && !module.loading) {
+        module.loading = true;
+        return requireAll(module.deps, cb);
+      }
+      return cb();
+    }
+
+    var path = dep + '.js';
+    loadDep(path, function() {
+      requireSingle(dep, cb);
+    });
+  }
+
+  function resolveModule(id) {
+    var module = modules[id];
+    if (module.resolving) {
+      return module.exports;
+    }
+    module.resolving = true;
+    return invokeModule(module);
+  }
+
+  function solve(deps, module) {
+    if (!deps) return null;
+    return deps.map(function(dep) {
+      if (dep === "module") {
+        return module;
+      }
+      if (dep === "exports") {
+        if (!module) {
+          return {};
+        }
+        if (!module.exports) {
+          module.exports = {};
+        }
+        return module.exports;
+      }
+      return resolveModule(dep);
+    });
+  }
+
+  function invokeModule(module) {
+    if (module.exports) {
+      return module.exports;
+    }
+    var solved = solve(module.deps, module);
+    var result = invoke(module.factory, solved);
+    if (result) {
+      module.exports = result;
+    }
+    return module.exports;
+
+  }
+
+  function invoke(factory, solved) {
+    return typeof factory == "function" ? factory.apply(null, solved) : factory;
+  }
+
+  function loadDep(path, cb) {
+    var script = document.createElement('script');
+    script.src = path;
+    script.onload = cb;
+    document.body.appendChild(script);
+  }
+})(this);
